8a0e405640ce8b0e45e7f5d84cc8e7b8998a394d
babaphemy <babaphemy@gmail.com>
2016-07-12 15:57:29 +0100

    response

--- tpay.php
+++ tpay.php
@@ -8,56 +8,77 @@ $submittedamt = $_SESSION['amt4hash'];
 
 ?>
 
-
 <body>
 <?php
-$json = dquery($submittedamt,$submittedref);
+$json = dquery($submittedamt, $submittedref);
 ?>
 <div class="container">
 
     <!-- row 3: article/aside -->
     <div class="row" style="text-align: center">
-        <div class="col-md-offset-4 col-md-4 col-md-offset-4">
-            <h2>TRANSACTION STATUS (Query Response)</h2>
+        <h2>TRANSACTION STATUS (Query Response)</h2>
+
+        <div class="col-md-4">
             <div class="thumbnail">
                 <div class="caption">
-                    <h4><span class="glyphicon glyphicon-pushpin"></span> <?php echo ($json['ResponseCode']); ?> </h4>
-                    <p><?php echo ($json['ResponseDescription']);?></p>
-                    <P>RAW RESPONSE<br /><?php print_r($json) ;?></P>
+                    <h4>
+                        <span class="glyphicon glyphicon-pushpin"></span>
+                        <?php echo($json['ResponseCode']); ?> </h4>
+                    <p><?php echo($json['ResponseDescription']); ?></p>
                 </div>
             </div>
 
-            <?php
+        </div>
+    <div class="col-md-4">
 
-            if ($json['ResponseCode'] == "00"){?>
-            <img src="css/thankyou.jpg" alt="ok" />
-            <!-- nested row 3a: callouts -->
-            <div class="row">
-                    <div class="thumbnail">
-                        <div class="caption">
-                            <h4><span class="glyphicon glyphicon-pushpin"></span>
-                            <?php echo ($json['ResponseCode']); ?> </h4>
-                            <p><?php echo ($json['ResponseDescription']);?></p>
 
-                    </div>
+        <div class="thumbnail">
+            <div class="caption">
+                <P>RAW RESPONSE<br/><?php print_r($json); ?></P>
+            </div>
+        </div>
+
+
+    </div>
+
+
+    <div class="col-md-4">
+
+        <?php
+
+        if ($json['ResponseCode'] == "00"){
+        ?>
+
+        <!-- nested row 3a: callouts -->
+
+            <div class="thumbnail">
+                <div class="caption">
+                    <span class="glyphicon glyphicon-ok"></span>
+                    <img src="css/thankyou.jpg" alt="ok"/>
+
+
+
+
+
+
+            <?php } else { ?>
+                <img src="css/notok.jpg" alt="not ok"/>
+            <?php } ?>
                 </div>
-
-
-                <?php } else {?>
-                    <img src="css/notok.jpg" alt="not ok" />
-                <?php } ?>
 
         </div>
 
     </div>
+    </div>
+    </div>
+    <!-- end container -->
 
-</div> <!-- end container -->
-
-<!-- javascript -->
-<script src="http://code.jquery.com/jquery-latest.min.js"></script>
-<script src="js/bootstrap.min.js"></script>
-<script>
-    $('a.btn-info').tooltip()
-</script>
+    <!-- javascript -->
+    <script src="http://code.jquery.com/jquery-latest.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script>
+        $('a.btn-info').tooltip()
+    </script>
 </body>
+
 </html>
